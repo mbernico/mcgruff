851f824a582ae9c74f2495613c88abb8af55b59f
Mike Bernico <michael.bernico.qepz@statefarm.com>
2018-06-05 10:43:11 -0500

    added video conversion starter

---
+++ convert_video_to_images.py
@@ -0,0 +1,12 @@
+from util.video_tools import video_to_images
+
+
+def main():
+    """
+    use video_to_image() to convert the video to image files
+    :return:
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
